819daa285958db4c4439595ee9ba6a9271be9c14
Esteban <esteban.higon@sistemasgenomicos.com>
2024-12-18 22:34:20 +0100

    added moving update window while updating

--- app/src/main/java/com/estivy/sokkerarchitect/ui/screens/Login.kt
+++ app/src/main/java/com/estivy/sokkerarchitect/ui/screens/Login.kt
@@ -27,7 +27,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
-import java.util.concurrent.CompletableFuture
 
 
 @Composable
