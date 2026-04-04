ba3cdea35145eb3142e3dc2341dbef2645d626be
Esteban <esteban.higon@gmail.com>
2026-04-04 20:43:07 +0200

    version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.estivy.sokkerarchitect"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.9"
+        versionCode = 11
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
